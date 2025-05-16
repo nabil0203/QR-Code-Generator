dd56d1b175a0077699d03123b397214f05d8a2e8
Chowdhury Nabil Ahmed <nabilahmed0203@gmail.com>
2025-05-16 17:27:30 +0600

    Qr code generator

--- qrCode_generator.py
+++ qrCode_generator.py
@@ -1,18 +1,29 @@
-import qrcode
-#Library - make(data text value url{a function}) --> qrcode
-
-text = input("Enter the Text or Url: ")
-
-fileName = input("Enter File name with jpg/png Extension: ")
+import qrcode               # importing the qrcode library
 
 
-def generate_qr__code(text, fileName):
+# text = input("Enter the Text or Url: ")
+# fileName = input("Enter File name with jpg/png Extension: ")
+
+
+# function
+def generate_qr_code(filePath):
     
-    #Convert text or url to qrcode
-    image_qrcode = qrcode.make(text)  
+    # Handling the file
+    my_file = open(filePath, "r")
+    line = my_file.readlines()
+    my_file.close()
+
+    # extracting the input of input.txt
+    text = line[0].strip()
+    fileName = line[1].strip()
+
+
+    #Convert text/url to qrcode
+    image_qrcode = qrcode.make(text)
 
     #Save QR code Image
     image_qrcode.save(fileName)
 
 
-generate_qr__code(text, fileName)
+# funtion call
+generate_qr_code("input.txt")
